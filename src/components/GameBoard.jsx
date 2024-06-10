3538f85a858c843e056fb255b73ec189025c2f4e
SolPyDev <108149462+SolPyDev@users.noreply.github.com>
2024-06-10 17:39:45 -0400

    confetti and sound effects

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import KittenCard from "./KittenCard";
 import { getRandomKittens } from "../services";
+import Confetti from 'react-confetti';
 
 const GameBoard = () => {
   const [numOfKittens, setNumOfKittens] = useState(6);
@@ -11,10 +12,12 @@ const GameBoard = () => {
   const [cardTwo, setCardTwo] = useState(null);
   const [solvedKittensArray, setSolvedKittensArray] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [gameWin, setGameWin] = useState(false)
 
   // New game
   const newGame = async () => {
     setLoading(true);
+    setGameWin(false);
     setKittens([]);
     setCardOne(null);
     setCardTwo(null);
@@ -55,15 +58,16 @@ const GameBoard = () => {
 
   // Function to check matches
   useEffect(() => {
-    if (cardTwo === null) return;
-    const timeout = setTimeout(() => {
-      if (cardOne?.url === cardTwo?.url && cardOne?.id != cardTwo?.id) {
-        setSolvedKittensArray((prev) => [...prev, cardOne.url])
-      }
-      setCardOne(null);
-      setCardTwo(null);
-    }, 500)
-    return () => clearTimeout(timeout)
+    if (cardTwo != null) {
+      const timeout = setTimeout(() => {
+        if (cardOne?.url === cardTwo?.url && cardOne?.id != cardTwo?.id) {
+          setSolvedKittensArray((prev) => [...prev, cardOne.url])
+        }
+        setCardOne(null);
+        setCardTwo(null);
+      }, 500)
+      return () => clearTimeout(timeout)
+    }
   }, [cardTwo])
 
   useEffect(() => {
@@ -72,61 +76,81 @@ const GameBoard = () => {
 
   useEffect(() => {
     newGame();
-  }, [numOfKittens] )
+  }, [numOfKittens]);
+
+  useEffect(() => {
+    if (solvedKittensArray.length === numOfKittens) {
+      setGameWin(true);
+      document.getElementById('meow').play();
+    }
+  }, [solvedKittensArray])
 
   return (
-    <div className="flex flex-col item-center justify-center w-full max-w-screen-2xl">
-      <div className="flex w-full items-center justify-between mx-auto p-6">
-        <button
-          className="btn w-fit"
-          onClick={() => {
-            newGame();
-          }}
-        >
-          New Game
-        </button>
-        <details id="num_kittens" className="dropdown dropdown-end">
-          <summary className="m-1 btn">Number of Kittens</summary>
-          <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-300 rounded-box w-52">
-            {[6, 8, 10, 12].map((number) => (
-              <li
-                key={number}
-                onClick={() => {
-                  setNumOfKittens(number);
-                  document
-                    .getElementById("num_kittens")
-                    .removeAttribute("open");
-                }}
-              >
-                <a>{number}</a>
-              </li>
-            ))}
-          </ul>
-        </details>
-      </div>
-      <div className="flex flex-row items-center justify-center flex-wrap gap-6 mb-12">
-        {!loading && kittens.length > numOfKittens ? (
-          kittens.map((kitten) => (
-            <KittenCard
-              key={kitten.id}
-              kitten={kitten}
-              setCardOne={setCardOne}
-              setCardTwo={setCardTwo}
-              cardOne={cardOne}
-              cardTwo={cardTwo}
-              solvedKittensArray={solvedKittensArray}
-              setSolvedKittensArray={setSolvedKittensArray}
-            />
-          ))
-        ) : (
-          <p className="flex gap-2 justify-center items-center text-2xl font-bold">
-            loading kittens{" "}
-            <span className="loading loading-dots loading-lg"></span>
-          </p>
-        )}
-      </div>
-    </div>
-  );
+		<div className="flex flex-col item-center justify-center w-full max-w-screen-2xl">
+			<audio id="meow" src="/cat-meow.mp3" />
+			{gameWin && (
+				<div className="fixed top-0 left-0 right-0 z-[100]">
+					<Confetti
+						confettiSource={{ x: 0, y: 50, w: window.innerWidth, h: 0 }}
+						width={window.width}
+						height={window.height}
+						recycle={false}
+						numberOfPieces={1000}
+						onConfettiComplete={() => setGameWin(false)}
+					/>
+				</div>
+			)}
+			<div className="flex w-full items-center justify-between mx-auto p-6">
+				<button
+					className="btn w-fit"
+					onClick={() => {
+						newGame();
+					}}
+				>
+					New Game
+				</button>
+				<details id="num_kittens" className="dropdown dropdown-end">
+					<summary className="m-1 btn">Number of Kittens</summary>
+					<ul className="p-2 shadow menu dropdown-content z-[1] bg-base-300 rounded-box w-52">
+						{[6, 8, 10, 12].map((number) => (
+							<li
+								key={number}
+								onClick={() => {
+									setNumOfKittens(number);
+									document
+										.getElementById('num_kittens')
+										.removeAttribute('open');
+								}}
+							>
+								<a>{number}</a>
+							</li>
+						))}
+					</ul>
+				</details>
+			</div>
+			<div className="flex flex-row items-center justify-center flex-wrap gap-6 mb-12">
+				{!loading && kittens.length > numOfKittens ? (
+					kittens.map((kitten) => (
+						<KittenCard
+							key={kitten.id}
+							kitten={kitten}
+							setCardOne={setCardOne}
+							setCardTwo={setCardTwo}
+							cardOne={cardOne}
+							cardTwo={cardTwo}
+							solvedKittensArray={solvedKittensArray}
+							setSolvedKittensArray={setSolvedKittensArray}
+						/>
+					))
+				) : (
+					<p className="flex gap-2 justify-center items-center text-2xl font-bold">
+						loading kittens{' '}
+						<span className="loading loading-dots loading-lg"></span>
+					</p>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default GameBoard;
